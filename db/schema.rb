7081f1917c76820a7dd925a02c6cedcdf2a5ff0d
David Villegas <david.villegasg7@gmail.com>
2020-05-23 15:45:13 -0400

    Testing has_and_belongs_to_many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_03_004043) do
+ActiveRecord::Schema.define(version: 2020_05_17_041751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,11 +36,28 @@ ActiveRecord::Schema.define(version: 2020_05_03_004043) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "courses_students", id: false, force: :cascade do |t|
+    t.bigint "course_id", null: false
+    t.bigint "student_id", null: false
+  end
+
   create_table "desserts", force: :cascade do |t|
     t.string "name"
     t.string "price"
     t.integer "stock"
     t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
